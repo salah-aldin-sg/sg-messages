288c33a215e408fd22cef65a97f33632a9036af5
Salah-Aldin Amera <salah.aldin.dev@gmail.com>
2026-04-15 18:20:20 +0000

    Initial Spring Boot project import from 1-init

---
+++ src/test/java/com/smartglobe/sgmessages/SgMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartglobe.sgmessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
